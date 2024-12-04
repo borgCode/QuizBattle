b58d2f8f142b272d0bcb565cc7b3aa1049d5b6e6
Kieran Borg <kierans.borg@gmail.com>
2024-12-04 21:43:43 +0100

    Scorewindow layout

--- frontend/src/app/components/multiplayer/multiplayer-score-window/multiplayer-score-window.component.ts
+++ frontend/src/app/components/multiplayer/multiplayer-score-window/multiplayer-score-window.component.ts
@@ -2,15 +2,28 @@ import {Component, OnInit} from '@angular/core';
 import {MultiplayerService} from '../../../services/services/multiplayer.service';
 import {ActivatedRoute} from '@angular/router';
 import {MultiplayerSessionDto} from '../../../services/models/multiplayer-session-dto';
+import {NgForOf} from '@angular/common';
+
+interface Box {
+  color: string;
+}
+
+interface BoxRow {
+  left: Box[];
+  right: Box[];
+}
 
 @Component({
   selector: 'app-multiplayer-score-window',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './multiplayer-score-window.component.html',
   styleUrl: './multiplayer-score-window.component.css'
 })
 export class MultiplayerScoreWindowComponent implements OnInit{
   gameSession!:MultiplayerSessionDto;
+  boxes: BoxRow[] = [];
 
   constructor(
     private multiplayerService: MultiplayerService,
@@ -21,10 +34,25 @@ export class MultiplayerScoreWindowComponent implements OnInit{
   }
 
   ngOnInit() {
+    this.initBoxes();
     this.activatedRoute.params.subscribe(value => {
       const sessionId = value['id'];
     });
   }
+
+  private initBoxes() {
+    for (let i = 0; i < 6; i++) {
+      this.boxes.push({
+        left: Array(3).fill({ color: '#ccc' }),
+        right: Array(3).fill({ color: '#ccc' })
+      });
+    }
+    }
+
+  updateBoxColor(side: 'left' | 'right', rowIndex: number, colIndex: number, color: string) {
+    this.boxes[rowIndex][side][colIndex].color = color;
+  }
+
 
 
 }
