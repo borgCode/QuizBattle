5ba1be98ce8702af2cd05b2bc3a87dec40ab8756
Kieran Borg <kierans.borg@gmail.com>
2024-12-04 13:08:39 +0100

    basic multiplayer page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {UserProfileComponent} from './components/user-profile/user-profile.component';
-import {PlayComponent} from './components/play/play.component';
 import {ModeSelectionComponent} from './components/mode-selection/mode-selection.component';
 import {RegisterComponent} from './components/register/register.component';
 import {authGuard} from './services/guard/auth.guard';
+import {MultiplayerComponent} from './components/multiplayer/multiplayer.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'profile', component: UserProfileComponent, canActivate:[authGuard]},
-  {path: 'play', component: PlayComponent, canActivate:[authGuard]},
+  {path: 'multiplayer', component: MultiplayerComponent, canActivate:[authGuard]},
   {path: '', component: ModeSelectionComponent}
 ];
