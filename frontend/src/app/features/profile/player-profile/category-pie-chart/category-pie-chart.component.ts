3e473468092237ce185f1a6cf471d00c11c796d4
Kieran Borg <kierans.borg@gmail.com>
2024-12-11 17:58:14 +0100

    Category pie chart done

--- frontend/src/app/features/profile/player-profile/category-pie-chart/category-pie-chart.component.ts
+++ frontend/src/app/features/profile/player-profile/category-pie-chart/category-pie-chart.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {ArcElement, Chart, PieController} from 'chart.js';
-Chart.register(PieController, ArcElement)
+import {Component, Input, OnInit} from '@angular/core';
+import {ArcElement, Chart, PieController, plugins} from 'chart.js';
+
+Chart.register(PieController, ArcElement, plugins)
 
 @Component({
   selector: 'app-category-pie-chart',
@@ -9,34 +10,57 @@ Chart.register(PieController, ArcElement)
   styleUrl: './category-pie-chart.component.css'
 })
 export class CategoryPieChartComponent implements OnInit {
+  @Input() categoryStats!: { [key: string]: any };
+  mapKeys: string[] = [];
   public chart: any;
 
 
   ngOnInit() {
+    this.mapKeys = Object.keys(this.categoryStats);
     this.createChart();
+    console.log(this.categoryStats)
   }
 
   private createChart() {
     this.chart = new Chart("MyChart", {
       type: 'pie',
       data: {
-        labels: ['Red', 'Pink', 'Green', 'Yellow', 'Orange', 'Blue',],
+        labels: this.mapKeys,
         datasets: [{
-          label: 'My First Dataset',
-          data: [300, 240, 100, 432, 253, 34],
+          label: 'Wins',
+          data: this.mapKeys.map(key => this.categoryStats[key].wins),
           backgroundColor: [
-            'red',
-            'pink',
-            'green',
-            'yellow',
-            'orange',
-            'blue',
+            '#FF0000',
+            '#00FF00',
+            '#0000FF',
+            '#FFFF00',
+            '#00FFFF',
+            '#FF00FF',
+            '#FFA500'
           ],
           hoverOffset: 4
         }],
       },
       options: {
-        aspectRatio: 2.5
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Category performance',
+            font: {
+              size: 50
+            },
+            color: 'black',
+          },
+          legend: {
+            labels: {
+              font: {
+                size: 16
+              }
+            }
+          }
+        }
       }
 
     });
