501a7f8d5b60e763119df556b6fe394c2457a928
Kieran Borg <kierans.borg@gmail.com>
2025-01-08 10:28:20 +0100

    messaging relationships

---
+++ backend/src/main/java/org/borg/backend/chat/model/Message.java
@@ -0,0 +1,32 @@
+package org.borg.backend.chat.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.borg.backend.player.model.Player;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+    
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Player sender;
+    
+    private Instant sentAt;
+    private boolean isRead;
+    private String content;
+}
---
+++ backend/src/main/java/org/borg/backend/chat/service/MessagingService.java
@@ -0,0 +1,7 @@
+package org.borg.backend.chat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessagingService {
+}
