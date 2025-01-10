9e8339ea1fa164e943866f7fe995e38133617a87
Kieran Borg <kierans.borg@gmail.com>
2025-01-10 22:31:13 +0100

    Player dropdown

--- frontend/src/app/core/components/navbar/player-dropdown/player-dropdown.component.ts
+++ frontend/src/app/core/components/navbar/player-dropdown/player-dropdown.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PlayerDto} from '../../../../api/generated/models/player-dto';
+import {LoginStateService} from '../../../services/login-state-service/login-state.service';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {TokenService} from '../../../services/token/token.service';
 
 @Component({
   selector: 'app-player-dropdown',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './player-dropdown.component.html',
   styleUrl: './player-dropdown.component.css'
 })
-export class PlayerDropdownComponent {
+export class PlayerDropdownComponent implements OnInit {
+  player: PlayerDto;
 
+  constructor(
+    private loginStateService: LoginStateService,
+    private router: Router,
+    private tokenService: TokenService
+  ) {
+  }
+
+  ngOnInit() {
+    this.player = this.loginStateService.loggedInUser;
+  }
+
+  logout() {
+    this.tokenService.clearTokens();
+    this.loginStateService.clearLoggedInUser();
+    this.router.navigate(['/login']);
+  }
 }
